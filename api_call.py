737db94143093c987a4a57e603308cc7aa469ce0
Ethan Edwards <ethanedwards@Ethans-MacBook-Air.local>
2023-09-28 15:17:30 -0400

    Changed the async test to use gather and now it runs correctly

--- api_call.py
+++ api_call.py
@@ -1,8 +1,9 @@
 import asyncio
+import aiohttp
 from aiohttp import ClientSession
 import openai
 import config
-
+import time
 
 # The rate limit for OpenAI API
 RATE_LIMIT = 300
@@ -22,24 +23,94 @@ async def end_async():
     await openai.aiosession.get().close()
 
 
-async def api_call(prompt: str, model: str='gpt-4', temperature: float=0.0, max_tokens: int=100, messages: list=[]):
+async def api_call(prompt: str, model: str='gpt-4', temperature: float=0.0, max_tokens: int=100, messages: list=[], max_retries: int=3):
     # Obtain a semaphore
     async with semaphore:
-        result = await create_chat_completion(prompt, model, temperature, max_tokens, messages)
+        result = await create_chat_completion(prompt, model, temperature, max_tokens, messages, max_retries)
 
     return result
 
 
-async def create_chat_completion(prompt: str, model: str='gpt-4', temperature: float=0.0, max_tokens: int=100, messages: list=[]):
+async def create_chat_completion(prompt: str, model: str='gpt-4', temperature: float=0.0, max_tokens: int=100, messages: list=[], max_retries: int=3):
     if not messages:  # if messages list is empty
         messages = [
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": prompt}
         ]
-    chat_completion_resp = await openai.ChatCompletion.acreate(
-        model=model, 
-        messages=messages,
-        temperature=temperature,
-        max_tokens=max_tokens
-        )
-    return chat_completion_resp['choices'][0]['message']['content']
+
+    for i in range(max_retries):
+        try:
+            chat_completion_resp = await openai.ChatCompletion.acreate(
+                model=model, 
+                messages=messages,
+                temperature=temperature,
+                max_tokens=max_tokens
+                )
+            
+            return chat_completion_resp['choices'][0]['message']['content']
+            
+        except aiohttp.ClientError as e:
+            print(f"Attempt {i+1}/{max_retries} failed with error: {e}")
+
+            if i < max_retries - 1:  # If not the final attempt
+                await asyncio.sleep(5)  # Wait for 5 seconds before retrying
+            else:
+                print("All attempts failed. Raising last captured exception.")
+                raise  # Re-raise the last exception
+
+        except openai.error.APIError as e:
+        #Handle API error here, e.g. retry or log
+            print(f"OpenAI API returned an API Error: {e}")
+            print(f'Last attempt was {prompt}')
+            pass
+        except openai.error.APIConnectionError as e:
+            #Handle connection error here
+            print(f"Failed to connect to OpenAI API: {e}")
+            print(f'Last attempt was {prompt}')
+            pass
+        except openai.error.RateLimitError as e:
+        #Handle rate limit error (we recommend using exponential backoff)
+            print(f"OpenAI API request exceeded rate limit: {e}")
+            print(f'Last attempt was {prompt}')
+            pass
+
+#Not asynchronous, use for testing
+def create_chat_completion_sync(prompt: str, model: str='gpt-4', temperature: float=0.0, max_tokens: int=100, messages: list=[], max_retries: int=3):
+    if not messages:  # if messages list is empty
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+
+    for i in range(max_retries):
+        try:
+            chat_completion_resp = openai.ChatCompletion.create(
+                model=model, 
+                messages=messages,
+                temperature=temperature,
+                max_tokens=max_tokens
+                )
+            
+            return chat_completion_resp['choices'][0]['message']['content']
+            
+        except openai.error.Timeout:
+            if i < max_retries - 1:  # i.e. if it's not the final attempt
+                time.sleep(5)
+            else:
+                print(f'Last attempt was {prompt}')
+                raise  # re-throw the last exception if all attempts fail
+        except openai.error.APIError as e:
+        #Handle API error here, e.g. retry or log
+            print(f"OpenAI API returned an API Error: {e}")
+            print(f'Last attempt was {prompt}')
+            pass
+        except openai.error.APIConnectionError as e:
+            #Handle connection error here
+            print(f"Failed to connect to OpenAI API: {e}")
+            print(f'Last attempt was {prompt}')
+            pass
+        except openai.error.RateLimitError as e:
+        #Handle rate limit error (we recommend using exponential backoff)
+            print(f"OpenAI API request exceeded rate limit: {e}")
+            print(f'Last attempt was {prompt}')
+            pass
--- tests.py
+++ tests.py
@@ -1,32 +1,47 @@
-from api_call import begin_async, end_async, api_call
+from api_call import begin_async, end_async, api_call, create_chat_completion_sync
 import asyncio
 import random
 import time
 
-
-async def testAsync():
+async def test_async():
     begin_async()
+    start = time.time()
+    tasks = []
+
+    for i in range(5):
+        for j in range(5):
+            operator = random.choice(['+', '-', '*', '/'])
+            prompt = str(i) + operator + str(j) + '='
+            tasks.append(api_call(prompt, model='gpt-3.5-turbo'))
+    results = await asyncio.gather(*tasks)
+    
+    for result in results:
+        print(result)
+
+    end = time.time()
+    print(f'Completion time is {end - start}')
+    await end_async()
+
+def test_sync():
     #set up timer
     start = time.time()
     #iterate through two one-digit random number lists as operands
-    for i in range(10):
-        for j in range(10):
+    for i in range(5):
+        for j in range(5):
             #generate random operator
             operator = random.choice(['+', '-', '*', '/'])
             #generate prompt
             prompt = str(i) + operator + str(j) + '='
             #call api
             #use turbo to save on credits
-            result = await api_call(prompt, model='gpt-3.5-turbo')
+            result = create_chat_completion_sync(prompt, model='gpt-3.5-turbo')
             print(result)
 
     #end timer
     end = time.time()
     print(f'Completion time is {end - start}')
 
-    #close session
-    await end_async()
-
 #main function
 if __name__ == '__main__':
-    asyncio.run(testAsync())
+    asyncio.run(test_async2())
+    #test_sync()
